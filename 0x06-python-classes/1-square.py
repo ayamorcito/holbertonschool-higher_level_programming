591cc61d79cfaac75b47e7b83ffa724565630533
ayamorcito <izquierdoz.g@gmail.com>
2022-05-17 19:48:51 -0700

    1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""define size of a square by its input"""
+
+
+class Square:
+    """create square class"""
+    def __init__(self, size):
+        """define private size with size"""
+        self.__size = size
