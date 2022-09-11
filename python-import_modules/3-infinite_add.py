96de0f2aae9182c300eb250b9634b1fa2a32ce56
ayamorcito <izquierdoz.g@gmail.com>
2022-09-11 16:39:13 -0700

    3full

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv)
+    result = 0
+    for i in range(1, argc):
+        result += int(argv[i])
+    print(result)
