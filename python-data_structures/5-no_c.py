11896b2bbbc9d14c921ec0410fcbb0bb14efb2d9
ayamorcito <izquierdoz.g@gmail.com>
2022-09-18 15:31:34 -0700

    11 full

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    i = 0
+    mystr = ""
+
+    while i < len(my_string):
+        if (my_string[i] != 'c' and my_string[i] != 'C'):
+            mystr += my_string[i]
+        i += 1
+    return (mystr)
