f5651cdd9a66632197268943e3befd98ac72a693
ayamorcito <izquierdoz.g@gmail.com>
2022-09-18 15:43:31 -0700

    6fix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for x in matrix:
         array_len = len(x)
         for y in range(array_len):
-            print("{}{}".format(x[y], ' ' if y != array_len - 1 else ''),
+            print("{:d}{:d}".format(x[y], ' ' if y != array_len - 1 else ''),
                   end='')
         print()
