625ecd3f0ef277fecadbcc5ca129048591438a24
ayamorcito <izquierdoz.g@gmail.com>
2022-09-22 12:07:03 -0700

    1full

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class based on 0-square.py with private size"""
+
+
+class Square:
+    """creates a square from class"""
+    def __init__(self, size):
+        """defines the size"""
+        self.__size = size
