1fa9b8848a33a6dc71e33b9b79fac96e2710d610
ayamorcito <izquierdoz.g@gmail.com>
2022-09-22 12:19:25 -0700

    2full

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""writes a class Square that defines a sq based on 1-square.py"""
+
+
+class Square:
+    """creates a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an interger")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
