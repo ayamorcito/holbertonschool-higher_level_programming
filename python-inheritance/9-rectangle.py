d52cc5b603f836818795e8e7cfa29ec53eaa4544
ayamorcito <izquierdoz.g@gmail.com>
2022-09-28 12:21:35 -0700

    9fix

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(BaseGeometry):
         self.integer_validator("height", height)
         self.__width = width
         self.__height = height
-    
+
     def area(self):
         """ area = width * height """
         return (self.__width * self.__height)
