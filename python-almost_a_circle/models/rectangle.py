4e9160eb6f8363fa98bd393c414a4f585f628a3a
amorcito <izquierdoz.g@gmail.com>
2022-10-07 08:32:07 -0700

    2fix

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -2,13 +2,13 @@
 """ Rectangle Class """
 
 
-from models.base import Base
+from .base import Base
 
 
 class Rectangle(Base):
     """ Rectangle Class """
 
-    def __init__(self, width, height, x=0, y=0, id=none):
+    def __init__(self, width, height, x=0, y=0, id=None):
         self.width = width
         self.height = height
         self.x = x
