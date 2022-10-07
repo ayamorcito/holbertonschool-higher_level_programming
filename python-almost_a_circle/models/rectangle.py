5ec2fc8727019c49a1e5510de287de2c8c6fc237
amorcito <izquierdoz.g@gmail.com>
2022-10-07 08:21:26 -0700

    2full

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+""" Rectangle Class """
+
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """ Rectangle Class """
+
+    def __init__(self, width, height, x=0, y=0, id=none):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """ width getter """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+
+    @property
+    def height(self):
+        """ height getter """
+        return self.__height
+
+    @height.setter
+    def width(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """ x getter """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """ y getter """
+        return self.y
+
+    @y.setter
+    def y(self, value):
+        self.__y = value
