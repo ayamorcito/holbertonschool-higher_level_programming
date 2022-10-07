9560f5b95e4e74fd2e803b5cde4c270012816da3
amorcito <izquierdoz.g@gmail.com>
2022-10-07 07:29:41 -0700

    1fixed

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """ base.py """
 
+
 class Base:
     """ base class of project """
-    
     __nb_objects = 0
 
-    
     def __init__(self, id=None):
-        
         if id is not None:
             self.id = id
         else:
