ddbe79ac14f0645655cb58bcada254e97530c86e
ayamorcito <izquierdoz.g@gmail.com>
2022-11-06 17:50:02 -0800

    7

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,8 +1,3 @@
--- Create database hbtn_0d_usa and table cities
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (
-    id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
-    state_id INT NOT NULL,
-    name VARCHAR(256) NOT NULL,
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id));
+-- Insert a new row in the table first_table
+INSERT INTO first_table (id, name)
+VALUES ('89', 'Best School');
