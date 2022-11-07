9a6b36de302906febb21d1f702f4dbbb3af4d198
ayamorcito <izquierdoz.g@gmail.com>
2022-11-06 18:06:06 -0800

    0 + readme

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- script lists all privileges of MySQL user_0d_1 & user_0d_2 in localhost.
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ SQL_more_queries/README.md
@@ -0,0 +1,1 @@
+In this repository i will upload the tasks for the SQL_more_queries objective
