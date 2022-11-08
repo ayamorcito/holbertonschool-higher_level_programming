c24a91afe90ff0ca6d46e4be0273a60a09fb93a3
ayamorcito <izquierdoz.g@gmail.com>
2022-11-08 08:36:17 -0800

    pycodefix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,14 @@
 """ script that lists all states from the database hbtn_0e_0_usa """
 
 
-import MySQLdb 
+import MySQLdb
 from sys import argv
 
 
 if __name__ == '__main__':
 
-
-    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1], password=argv[2], database=argv[3])
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                               password=argv[2], database=argv[3])
 
     cursor = database.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
@@ -21,4 +21,3 @@ if __name__ == '__main__':
 
     cursor.close()
     database.close()
-
