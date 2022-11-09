4e1f328cdec934c5ec8a3c0eb13e7201de5c8aa6
ayamorcito <izquierdoz.g@gmail.com>
2022-11-09 07:45:19 -0800

    1

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+""" script that lists all states from the database hbtn_0e_0_usa """
+
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+
+    database = MySQLdb.connect(host='localhost', port=3306, user=argv[1],
+                               password=argv[2], database=argv[3])
+
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    states = cursor.fetchall()
+
+    for row in states:
+        if row[1][0] == 'N':
+            print(row)
+
+    cursor.close()
+    database.close()
