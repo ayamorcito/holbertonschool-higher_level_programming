08720b36fbd43de9c5ab92538fe4ca94b90eef9a
ayamorcito <izquierdoz.g@gmail.com>
2022-11-13 17:47:53 -0800

    12

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Adds new state obj 6 Louisiana """
+
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, Session
+
+if __name__ == '__main__':
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(argv[1], argv[2], argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        state = session.query(State).get(2)
+        state.name = 'New Mexico'
+        session.commit()
