a3f5ca2e5927a66cf145728d867849eeff194954
ayamorcito <izquierdoz.g@gmail.com>
2022-12-14 11:03:50 -0800

    6fix

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const to_print = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < to_print.length; i++) {
-  console.log(to_print[i]);
+const toprint = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < toprint.length; i++) {
+  console.log(toprint[i]);
 }
